8628f9747b2f3c2d2ffd19591c2d9fc425eb23f6
Yumeka <nakano9544@gmail.com>
2025-11-27 06:21:37 +0700

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}", "./app/**/*.{js,jsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
